560f7807416b5f744825906d722227b2d5597f79
Matheus Dimer <matheus-delima@outlook.com>
2023-04-28 22:29:31 -0300

    Regra lexo analisador de char.

--- src/main/java/com/unesc/compilador/analisadorlexico/analisador/AnalisadorChar.java
+++ src/main/java/com/unesc/compilador/analisadorlexico/analisador/AnalisadorChar.java
@@ -2,6 +2,7 @@ package com.unesc.compilador.analisadorlexico.analisador;
 
 import com.unesc.compilador.analisadorlexico.base.AbstractAnalisadorExpressao;
 import com.unesc.compilador.analisadorlexico.base.PointerController;
+import com.unesc.compilador.analisadorlexico.base.RegraLexaException;
 import com.unesc.compilador.analisadorlexico.base.Token;
 
 import java.util.regex.Pattern;
@@ -14,6 +15,16 @@ public class AnalisadorChar extends AbstractAnalisadorExpressao {
 
     @Override
     public Token analisar(PointerController controller) {
-        return null;
+        char char1 = controller.getNext();
+        char char2 = controller.getNext();
+        char char3 = controller.getNext();
+
+        String token = "" + char1 + char2 + char3;
+
+        if (char3 != '\'') {
+            throw new RegraLexaException("Esperado ' para fechamento do caracter, porém foi dado " + char3, token, controller.getRow());
+        }
+
+        return new Token(token, 39, controller.getRow());
     }
 }
