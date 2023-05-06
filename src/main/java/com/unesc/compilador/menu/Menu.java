083f071d3379893c7fc44351e25f017101fea753
BETHA\rai.martins <raibmartins@betha.com.br>
2023-05-05 22:01:12 -0300

    tela bonita

--- src/main/java/com/unesc/compilador/menu/Menu.java
+++ src/main/java/com/unesc/compilador/menu/Menu.java
@@ -5,6 +5,9 @@ import com.unesc.compilador.analisadorlexico.base.RegraLexaException;
 import com.unesc.compilador.analisadorlexico.base.Token;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Element;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,24 +16,57 @@ import java.util.stream.Collectors;
 
 public class Menu extends JFrame implements ActionListener {
     private final JTextArea textArea;
+    private final JTextArea lines;
     private final JButton button;
+    private final JScrollPane jsp;
 
     public Menu() {
-        super("Digite o código");
+        setTitle("Digite o código");
+        jsp = new JScrollPane();
+        textArea = new JTextArea();
+        lines = new JTextArea("1");
+        lines.setBackground(Color.LIGHT_GRAY);
+        lines.setEditable(false);
+        //  Code to implement line numbers inside the JTextArea
+        textArea.getDocument().addDocumentListener(new DocumentListener() {
+            public String getText() {
+                int caretPosition = textArea.getDocument().getLength();
+                Element root = textArea.getDocument().getDefaultRootElement();
+                String text = "1" + System.getProperty("line.separator");
+                for(int i = 2; i < root.getElementIndex(caretPosition) + 2; i++) {
+                    text += i + System.getProperty("line.separator");
+                }
+                return text;
+            }
+            @Override
+            public void changedUpdate(DocumentEvent de) {
+                lines.setText(getText());
+            }
+            @Override
+            public void insertUpdate(DocumentEvent de) {
+                lines.setText(getText());
+            }
+            @Override
+            public void removeUpdate(DocumentEvent de) {
+                lines.setText(getText());
+            }
+        });
+        jsp.getViewport().add(textArea);
+        jsp.setRowHeaderView(lines);
+        add(jsp);
+        setSize(400, 275);
 
-        textArea = new JTextArea(10, 40);
-        JScrollPane scrollPane = new JScrollPane(textArea);
+
 
         button = new JButton("Analisar");
         button.addActionListener(this);
 
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
-        panel.add(scrollPane, BorderLayout.CENTER);
+        panel.add(jsp, BorderLayout.CENTER);
         panel.add(button, BorderLayout.SOUTH);
 
         setContentPane(panel);
-        pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
